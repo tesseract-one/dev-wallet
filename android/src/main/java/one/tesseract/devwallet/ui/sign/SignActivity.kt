67a1a16a8d732aee4936536b55738e82d1b0aae4
Daniel Leping <dileping@gmail.com>
2023-01-18 05:38:36 +0000

    put fragment in activity

--- android/src/main/java/one/tesseract/devwallet/ui/sign/SignActivity.kt
+++ android/src/main/java/one/tesseract/devwallet/ui/sign/SignActivity.kt
@@ -2,9 +2,19 @@ package one.tesseract.devwallet.ui.sign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+
+import one.tesseract.devwallet.ui.settings.home.HomeFragment
 
 class SignActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if(savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true) //must be here. otherwise compat mode
+                replace(android.R.id.content, HomeFragment())
+            }
+        }
     }
 }
