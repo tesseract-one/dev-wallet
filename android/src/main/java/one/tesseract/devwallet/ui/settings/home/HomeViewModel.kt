2e5f787f657d4f870190c0254f85403e911e6fe0
Daniel Leping <dileping@gmail.com>
2023-01-18 04:06:10 +0000

    home binding test

--- android/src/main/java/one/tesseract/devwallet/ui/settings/home/HomeViewModel.kt
+++ android/src/main/java/one/tesseract/devwallet/ui/settings/home/HomeViewModel.kt
@@ -10,4 +10,8 @@ class HomeViewModel : ViewModel() {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
+    fun showSign() {
+        _text.value = "Sign"
+    }
 }
