3cd5a8df060229a47bfdbcd7dc38e9db87ff0c99
Daniel Leping <dileping@gmail.com>
2023-01-18 03:46:22 +0000

    made home fragment databindings compatible

--- android/src/main/java/one/tesseract/devwallet/ui/settings/home/HomeFragment.kt
+++ android/src/main/java/one/tesseract/devwallet/ui/settings/home/HomeFragment.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import one.tesseract.devwallet.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
@@ -22,17 +20,13 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        binding.model = viewModel
+        binding.lifecycleOwner = this
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
