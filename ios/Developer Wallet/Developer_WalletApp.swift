d0b93c0c1267071a07854e80da2ccefae9ee4251
Daniel Leping <dileping@gmail.com>
2023-01-28 17:24:55 +0000

    cleanup

--- ios/Developer Wallet/Developer_WalletApp.swift
+++ ios/Developer Wallet/Developer_WalletApp.swift
@@ -9,28 +9,15 @@ import SwiftUI
 
 @main
 struct Developer_WalletApp: App {
-    let core = try! Core(ui: UI(), dataDir: settingsFolder()!, transport: .none)
+    private let core: Core
     
     init() {
-        //let aaa = FileManager.default.temporaryDirectory.path()
-//        let url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "one.tesseract.Developer-Wallet.settings")
-        
-//        print(url ?? "!!!none!!!")
-        
-//        print("lalala")
-//        
-////        let url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "one.tessteract.Developer-Wallet.settings");
-//
-//        if let stringData = "test data".data(using: .utf8) {
-//            try? stringData.write(to: url!)
-//        }
-        
-        //Date(from: "Test")
+        self.core = try! Core(ui: UI(), dataDir: settingsFolder()!, transport: .none)
     }
     
     var body: some Scene {
         WindowGroup {
-            ContentView(core: core)
+            ContentView(core: self.core)
         }
     }
 }
