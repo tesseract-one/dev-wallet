4c1c4b0be8856c63e6cebc6c37d3ee26c452e77b
Daniel Leping <dileping@gmail.com>
2023-03-28 20:54:48 +0100

    better home screen style

--- android/src/main/java/one/tesseract/devwallet/ui/settings/home/HomeViewModel.kt
+++ android/src/main/java/one/tesseract/devwallet/ui/settings/home/HomeViewModel.kt
@@ -9,17 +9,16 @@ class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = """
-            Thanks for installing the Tesseract developer wallet!
+    Thanks for installing the Tesseract developer wallet!
             
-            It's designed to help you test your dApp integration with Wallets through Tesseract protocol.
+    It's designed to help you test your dApp integration with Wallets through Tesseract protocol.
             
-            Here, in the app itself, you won't find the balance or tokens like in the wallets designed to hold crypto. For the sake of simplicity, the app contains only settings to provide your testing private key (don't use the key where you keep your crypto!) and set up the "tesseract test" protocol if needed.
+    Here, in the app itself, you won't find the balance or tokens like in the wallets designed to hold crypto. For the sake of simplicity, the app contains only settings to provide your testing private key (don't use the key where you keep your crypto!) and set up the "tesseract test" protocol if needed.
             
-            Currently supported protocols:
-            
-            - Substrate/Polkadot
-            - Tesseract Test protocol (to test tesseract connectivity)
-        """
+    Currently supported protocols:        
+        - Substrate/Polkadot
+        - Tesseract Test protocol (to test tesseract connectivity)
+"""
     }
     val text: LiveData<String> = _text
 
